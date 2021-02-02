eb177d8493734f2a8a688d60321004a36274773b
PTelnov <ptelnov@uclan.ac.uk>
2021-02-02 21:07:16 +0200

    Added links to the game list, which are not working right now, because they are need name of the player in the API call.

--- JavaScript/landingPageJS.js
+++ JavaScript/landingPageJS.js
@@ -1,17 +1,20 @@
 const gameList= document.getElementById("gameList");
 const button= document.getElementById("StButton");// address needed to delete button
+const chooseGame = document.getElementById("ChooseGame");// address of the h3 element with "Choose your game: content
 function getChallenges()
 {
+    button.remove();
+    chooseGame.style.display = 'block';
     fetch("https://codecyprus.org/th/api/list")
         .then(response => response.json()) //Parse JSON text to JavaScript object
         .then(jsonObject => {
             let treasureHunts = jsonObject.treasureHunts;
-            button.remove();
             for (let i = 0; i < treasureHunts.length; i++) {
                 let listItem = document.createElement("li");
                 let uID = treasureHunts[i].uuid;
                 console.log(uID);
-                listItem.innerHTML = treasureHunts[i].name;
+                let startLink = "https://codecyprus.org/th/api/start?player=...&app=TSAApp&treasure-hunt-id=" + uID; // Name needed
+                listItem.innerHTML = "<a href=" + startLink + ">" + treasureHunts[i].name + "</a>";
                 gameList.appendChild(listItem);
             }
         });
